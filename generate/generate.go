35ee6aca1258c82974e7eea4f8fb61ec0dc7bf45
Mike D Pilsbury <mike@pekim.co.uk>
2026-04-19 09:27:45 +0100

    Remove TranslationUnit type
    It's not necessary given there's only going to be one translation
    unit.

--- generate/generate.go
+++ generate/generate.go
@@ -1,8 +1,57 @@
 package generate
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"path"
+	"strings"
+	"sync"
+
+	"github.com/go-clang/clang-v15/clang"
+)
+
+type gen struct {
+	headerFile string
+	clang.TranslationUnit
+}
+
+var clangResourceDir = sync.OnceValue(func() string {
+	out, err := exec.Command("clang", "-print-resource-dir").Output()
+	fatalOnError(err)
+
+	resDir := strings.TrimSpace(string(out))
+	parts := strings.Split(resDir, "\n")
+	resDir = parts[0]
+
+	if resDir == "" {
+		fatal("no output when getting clang resource dir")
+	}
+	if !strings.HasPrefix(resDir, "/") {
+		fatalf("expected clang resource dir to start with '/', but it %s", resDir)
+	}
+
+	return resDir
+})
 
 func Generate() {
-	tu := newTranslationUnit("clang-c/Index.h")
-	fmt.Println(tu)
+	gen := gen{
+		headerFile: "clang-c/Index.h",
+	}
+	resourcesDir := clangResourceDir()
+	parseArgs := []string{
+		"-I", path.Join(resourcesDir, "include"),
+		"-x", "c-header",
+	}
+
+	index := clang.NewIndex(0, 1)
+	errCode := index.ParseTranslationUnit2(
+		gen.headerFile, parseArgs, nil,
+		clang.TranslationUnit_SkipFunctionBodies|clang.TranslationUnit_DetailedPreprocessingRecord,
+		&gen.TranslationUnit,
+	)
+	if errCode != clang.Error_Success {
+		fatal(errCode)
+	}
+
+	fmt.Println(gen)
 }
