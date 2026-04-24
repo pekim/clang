0920bafca02b90a6979679154498eb1300e4afdc
Mike D Pilsbury <mike@pekim.co.uk>
2026-04-24 12:28:39 +0100

    Add first function implementation

---
+++ function_test.go
@@ -0,0 +1,13 @@
+package clang
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFunctionNoArgsNoReturn(t *testing.T) {
+	assert.NotPanics(t, func() {
+		EnableStackTraces()
+	})
+}
